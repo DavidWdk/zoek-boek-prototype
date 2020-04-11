c1c6083ddc0f1b284d93610928fdca2fa7177f09
keesvlees2000 <david.widlak123@gmail.com>
2020-04-11 13:21:29 +0200

    Added more freelancers

--- finalFunc.js
+++ finalFunc.js
@@ -99,15 +99,148 @@ let anne = {
     distance: 33,
     prevShifts: 3,
     functions: [{name:'Jeugdzorg', color: '#5C0601', background: '#FF453A'}, 
+                {name:'Eerstelijnszorg', color: '#564500', background: '#FFCC00'},
+                {name: 'Tweedelijnszorg', color: '#12021A', background: '#BF5AF2'},
+                {name: 'Autismezorg', color: '#471b0e', background: '#FF7751'}]
+};
+
+let kees = {
+    id: 2,
+    bio: 'Dag, ik ben Kees. Ik ben 49 en heb vele jaren ervaring in de zorg. Mijn specialiteit is mijn brede kennis.',
+    mail: 'Kees@vandijk.nl',
+    phone: '06 242 91 282',
+    location: 'Roermond',
+    name: "Kees van Dijk",
+    score: 5.9,
+    tariff: 17.50,
+    reactionTime: 4,
+    distance: 87,
+    prevShifts: 2,
+    functions: [{name:'Jeugdzorg', color: '#5C0601', background: '#FF453A'}, 
+                {name:'Eerstelijnszorg', color: '#564500', background: '#FFCC00'},
+                {name: 'GGZ', color: '#00350D', background: '#34C759'},
+                {name: 'Autismezorg', color: '#471b0e', background: '#FF7751'}]
+};
+
+let joost = {
+    id: 2,
+    bio: 'Ik ben Joost te Paske, ik heb vele jaren kennis in de zorg en heb daarmee een sterke fundering opgebouwd rondom tweedelijnszorg, jeugdzorg, GGZ en austismezorg.',
+    mail: 'Joost@tepaske.nl',
+    phone: '06 891 93 223',
+    location: 'Eindhoven',
+    name: "Joost te Paske",
+    score: 6.1,
+    tariff: 35.00,
+    reactionTime: 12,
+    distance: 56,
+    prevShifts: 12,
+    functions: [{name: 'Tweedelijnszorg', color: '#12021A', background: '#BF5AF2'},
+                {name:'Jeugdzorg', color: '#5C0601', background: '#FF453A'}, 
+                {name: 'GGZ', color: '#00350D', background: '#34C759'},
+                {name: 'Autismezorg', color: '#471b0e', background: '#FF7751'}]
+};
+
+let tessa = {
+    id: 2,
+    bio: 'Ik vraag weinig voor mijn diensten, maar u krijgt er veel voor terug. Ik ben tevens erg flexibel en heb veel kennis rondom de onderstaande functies.',
+    mail: 'tessa@heuvels.nl',
+    phone: '06 221 99 283',
+    location: 'Rotterdam',
+    name: "Tessa Heuvels",
+    score: 7.8,
+    tariff: 29.00,
+    reactionTime: 10,
+    distance: 42,
+    prevShifts: 0,
+    functions: [{name:'Jeugdzorg', color: '#5C0601', background: '#FF453A'}, 
+                {name: 'Gehandicaptenzorg', color: '#1A4C4A', background: '#9DD9D2'},
+                {name:'Eerstelijnszorg', color: '#564500', background: '#90C2E7'},
+                {name: 'Tweedelijnszorg', color: '#12021A', background: '#BF5AF2'}]
+};
+
+let aulona = {
+    id: 2,
+    bio: 'Ik heb een MBO- en HBO-opleiding zorg gedaan, en heb daarmee veel kennis opgebouwd. Ik behandel graag kinderen en bejaarden, ook in uw zorginstelling.',
+    mail: 'aulona@dreshaj.nl',
+    phone: '06 290 12 132',
+    location: 'Rotterdam',
+    name: "Aulona Dreshaj",
+    score: 7.3,
+    tariff: 20,
+    reactionTime: 8,
+    distance: 53,
+    prevShifts: 14,
+    functions: [{name:'Jeugdzorg', color: '#5C0601', background: '#FF453A'}, 
+                {name: 'GGZ', color: '#00350D', background: '#34C759'},
+                {name:'Eerstelijnszorg', color: '#564500', background: '#90C2E7'},
+                {name: 'Bejaardenzorg', color: '#092327', background: '#00A9A5'}]
+};
+
+let sharmaine = {
+    id: 2,
+    bio: 'Ik ben een goedkope invalskracht maar verzeker u dat ik een uitstekende vervanger ben. Mijn prijs kwaliteit verhouding is erg hoog.',
+    mail: 'Sharmaine@janssen.nl',
+    phone: '06 123 78 883',
+    location: 'Wolvega',
+    name: "Sharmaine Janssen",
+    score: 6.2,
+    tariff: 17.50,
+    reactionTime: 2,
+    distance: 31,
+    prevShifts: 23,
+    functions: [{name: 'GGZ', color: '#00350D', background: '#34C759'},
+                {name: 'Gehandicaptenzorg', color: '#1A4C4A', background: '#9DD9D2'},
+                {name: 'PGB-Zorg', color: '#4D2D52', background: '#F08CAE'},
+                {name: 'Bejaardenzorg', color: '#092327', background: '#00A9A5'}]
+};
+
+let sjoerd = {
+    id: 2,
+    bio: 'Ik heb mijn bachalor verzorging behaald, en heb veel ervaring opgebouwd in de zorg als freelancer. Hiermee ben ik kwalitatief een van de beste vervangers voor uw zorgprofessional.',
+    mail: 'Sjoerd@Moonen.nl',
+    phone: '06 231 44 126',
+    location: 'Overdijk',
+    name: "Sjoerd Moonen",
+    score: 8.1,
+    tariff: 40.00,
+    reactionTime: 35,
+    distance: 57,
+    prevShifts: 9,
+    functions: [{name: 'Bejaardenzorg', color: '#092327', background: '#00A9A5'}, 
                 {name:'Eerstelijns', color: '#564500', background: '#FFCC00'},
                 {name: 'Tweedelijnszorg', color: '#12021A', background: '#BF5AF2'},
                 {name: 'Autismezorg', color: '#471b0e', background: '#FF7751'}]
 };
 
+let bas = {
+    id: 2,
+    bio: 'Ik ben Bas Rouing, een goede zorgprofessional met relatief veel ervaring. Ik sta altijd paraat uw klanten de beste verzorging te geven.',
+    mail: 'Bas@rouing.nl',
+    phone: '06 087 13 235',
+    location: 'Maastricht',
+    name: "Bas Rouing",
+    score: 5.1,
+    tariff: 22.50,
+    reactionTime: 5,
+    distance: 40,
+    prevShifts: 11,
+    functions: [{name:'Jeugdzorg', color: '#5C0601', background: '#FF453A'}, 
+                {name:'Eerstelijnszorg', color: '#564500', background: '#FFCC00'},
+                {name: 'Gehandicaptenzorg', color: '#1A4C4A', background: '#9DD9D2'},
+                {name: 'PGB-Zorg', color: '#4D2D52', background: '#F08CAE'}]
+};
+
 //pushes the freelancer data 
 freelancers.push(vera);
 freelancers.push(jan);
+freelancers.push(sjoerd);
 freelancers.push(anne);
+freelancers.push(tessa);
+freelancers.push(aulona);
+freelancers.push(sharmaine);
+freelancers.push(joost);
+freelancers.push(bas);
+freelancers.push(kees);
 
 //shows the searchresults: loops through every freelancer with the accessory data 
 function renderFreelancer() {
